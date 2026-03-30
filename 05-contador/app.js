13f61911a2ba69f5f6c684c2071c1fa3060bf6bf
MATHEUS LUCAS DE FREITAS ZACARIAS <25203742@CFP127ALUNO.LOCAL>
2026-03-30 15:14:16 -0300

    [feat] 05-contador finalizado

--- 05-contador/app.js
+++ 05-contador/app.js
@@ -1,6 +1,6 @@
 'use strict'
 
-function criarListaNumeros(quantidade) {
+const criarListaNumeros = function(quantidade) {
     let listaNumeros = []
 
     for (let i = 1; i <= quantidade; i++) {
@@ -9,10 +9,10 @@ function criarListaNumeros(quantidade) {
     return listaNumeros
 }
 
-function criarListaPar(quantidade) {
+const criarListaPar = function(quantidade) {
     let lista = []
     let qtdeDobro = (Number(quantidade) * 2)
-    let cont = 0
+    let cont = 1
 
 
     while (cont <= qtdeDobro) {
@@ -25,10 +25,10 @@ function criarListaPar(quantidade) {
     return lista
 }
 
-function criarListaImpar(quantidade) {
+const criarListaImpar = function(quantidade) {
     let lista = []
     let qtdeDobro = (Number(quantidade) * 2)
-    let cont = 0
+    let cont = 1
 
 
     while (cont <= qtdeDobro) {
@@ -41,8 +41,8 @@ function criarListaImpar(quantidade) {
     return lista
 }
 
-function criarMultiplosCinco(quantidade) {
-    let cont = 0
+const criarMultiplosCinco = function(quantidade) {
+    let cont = 1
     let lista = []
     let resultado
 
@@ -54,4 +54,53 @@ function criarMultiplosCinco(quantidade) {
     return lista
 }
 
-console.log(criarMultiplosCinco(10))
+const criarListaPotencia = function(quantidade){
+    let potecia = 2
+    let lista   = []
+    let resultado
+    for(let i = 0; i < quantidade; i++){
+       resultado = potecia**i
+        lista.push(resultado)
+        
+    }
+    return lista
+
+}
+
+function gerar(){
+    let tabela = document.getElementById('tabela')
+    tabela.innerHTML = ""
+    let quantidade = document.getElementById('quantidade').value
+    
+    
+
+    let pegarListaNumeros = criarListaNumeros(quantidade)
+    let pegarListaPar = criarListaPar(quantidade)
+    let pegarListaImpar = criarListaImpar(quantidade)
+    let pegarListaMultiploDeCinco = criarMultiplosCinco(quantidade)
+    let pegarListaPotencia = criarListaPotencia(quantidade)
+
+    
+    for(let i = 0; i < quantidade; i++){
+        const tr = document.createElement("tr")
+
+        const td1 = document.createElement("td")
+        const td2 = document.createElement("td")
+        const td3 = document.createElement("td")
+        const td4 = document.createElement("td")
+        const td5 = document.createElement("td")
+
+        
+        tr.append(td1, td2, td3, td4, td5)
+        tabela.appendChild(tr)
+
+        td1.textContent = pegarListaNumeros[i]
+        td2.textContent = pegarListaPar[i]
+        td3.textContent = pegarListaImpar[i]
+        td4.textContent = pegarListaMultiploDeCinco[i]
+        td5.textContent = pegarListaPotencia[i]
+
+    }
+
+
+}
